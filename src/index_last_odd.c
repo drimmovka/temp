79657bf3c2fc32e035b506c8fd74d27759feacff
KirillMaltsev3341 <144445070+KirillMaltsev3341@users.noreply.github.com>
2023-10-15 19:25:59 +0300

    Delete src directory

--- src/index_last_odd.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int index_last_odd(int array[], int size) {
-    for (int i = size-1; i >= 0; i--) {
-        if (abs(array[i]) % 2 != 0) return i;
-    }
-    return -1;
-}
--- src/index_last_odd.h
+++
@@ -1,6 +0,0 @@
-#ifndef INDEX_LAST_ODD_H
-#define INDEX_LAST_ODD_H
-
-int index_last_odd(int[], int );
-
-#endif
