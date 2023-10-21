7298c52eb05cbd737df086f7a4eda333d08846a5
KirillMaltsev3341 <144445070+KirillMaltsev3341@users.noreply.github.com>
2023-10-22 00:43:27 +0300

    Update main.c

--- wp/main.c
+++ wp/main.c
@@ -4,55 +4,17 @@
 
 void split_text(char*, char**);
 char* get_text(int*, int*);
-void fill_punctuation(char*, char*, int);
-void output_sentences(char**, char*, int n);
-int check_sentence(char*);
 
 int main() {
-    int text_size = 1, n = 0;
-    char* text = get_text(&text_size, &n);
+    int text_size = 1, array_size = 0;;
+    char* text = get_text(&text_size, &array_size);
+
+    printf("%ld\n", strlen(text));
+    printf("%d\n", array_size);
     
-    char* punctuation = malloc(n * sizeof(char));
-    fill_punctuation(text, punctuation, text_size);
-    
-    char** sentences = malloc(n * sizeof(char*));
-    split_text(text, sentences);
+    char* punctuation = malloc(array_size * sizeof(char));
+    char** array = malloc(array_size * sizeof(char*));
 
-    output_sentences(sentences, punctuation, n);
-}
-
-
-int check_sentence(char* sentence) {
-    return 1;
-    int count_upper = 0;
-    for (int i=0; i<strlen(sentence); i++) {
-        int code = (int) sentence[i];
-        if (65 <= code && code <= 90) count_upper++;
-    }
-    return (count_upper > 1);
-}
-
-void output_sentences(char** sentences, char* punctuation, int n) {
-    int m = 0;
-    for (int i=0; i<n; i++) {        
-        if (1) {
-            m++;
-            int flag = 0;
-            for (int j=0; j<strlen(sentences[i]); j++) {
-            if (sentences[i][j] != ' ' && sentences[i][j] != '\n')
-                flag = 1;
-            if (flag && sentences[i][j] != '\n') 
-                printf("%c", sentences[i][j]);
-        }
-        printf("%c\n", punctuation[i]);
-        }
-    }
-    printf("Dragon flew away!\n");
-    printf("Количество предложений до %d и количество предложений после %d\n", n, m);
-}
-
-
-void fill_punctuation(char* text, char* punctuation, int text_size) {
     int j = 0;
     for (int i=0; i<text_size-1; i++) {
         if (text[i] == '.' || text[i] == ';' || text[i] == '?') {
@@ -60,22 +22,37 @@ void fill_punctuation(char* text, char* punctuation, int text_size) {
             j++;
         }
     }
+
+    split_text(text, array);
+
+    for (int i=0; i<array_size; i++) {
+        int fl = 0;
+        for (int j=0; j<strlen(array[i]); j++) {
+            if (array[i][j] != ' ' && array[i][j] != '\n') fl = 1;
+            if (fl && array[i][j] != '\n') {
+                printf("%c", array[i][j]);
+            }
+        }
+        printf("%c\n", punctuation[i]);
+    }
+    printf("Dragon flew away!\n");
 }
 
-void split_text(char* text, char** sentences) {
+
+void split_text(char* text, char** array) {
     char* sep = ".;?";
     char* substr;
     
     substr = strtok(text, sep);
     int i = 0;
     while (substr != NULL) {
-        sentences[i] = substr;
+        array[i] = substr;
         i++;
         substr = strtok(NULL, sep);
     }
 }
 
-char* get_text(int* text_size, int* n) {
+char* get_text(int* text_size, int* array_size) {
     int capacity = 1;
     char* text = (char*) malloc(sizeof(char));
     char ch = getchar();
@@ -87,7 +64,7 @@ char* get_text(int* text_size, int* n) {
         }
         (*text_size)++;
         if (ch == '.' || ch == ';' || ch == '?') {
-            (*n)++;
+            (*array_size)++;
         }
         ch = getchar();
     }
