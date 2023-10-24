8cc30ee3f43fcf9d0f3afebac2a96dc96d7c01df
KirillMaltsev3341 <144445070+KirillMaltsev3341@users.noreply.github.com>
2023-10-24 20:02:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -11,110 +11,132 @@ type LinkedList struct {
 	Head *Node
 }
 
-func New(q int) *LinkedList {
-	// Инициализировать новый список. Количество элементов q.
+func New(size int) *LinkedList {
+	// Initialize a new list. The number of elements size.
 	Head := Node{}
 	current := &Head
-	for i := 0; i < q; i++ {
+	for i := 0; i < size-1; i++ {
 		current.Next = &Node{}
 		current = current.Next
 	}
 	return &LinkedList{Head: &Head}
 }
 
-func (l *LinkedList) Add(val int) {
-	// Добавить элемент в конец списка.
-	new_node := Node{Val: val, Next: nil}
-	current := l.Head
-	for i := 0; i < l.Size()-1; i++ {
-		current = current.Next
-	}
-	current.Next = &new_node
-}
-
-func (l *LinkedList) Pop() {
-	// Удалить элемент из конца.
-	current := l.Head
-	for i := 0; i < l.Size()-2; i++ {
-		current = current.Next
-	}
-	current.Next = nil
-}
-
-func (l *LinkedList) At(pos int) Node {
-	// Получение элемента из позиции pos.
-	current := l.Head
-	for i := 0; i < l.Size(); i++ {
-		if i == pos {
-			return *current
-		}
-		current = current.Next
-	}
-	return Node{}
-}
-
 func (l *LinkedList) Size() int {
-	// Вернуть длину списка
+	// Return the length of the list
 	size := 1
 	current := l.Head
-	for {
-		if current.Next == nil {
-			break
-		}
-		size += 1
+	for current.Next != nil {
+		size++
 		current = current.Next
 	}
 	return size
 }
 
-func (l *LinkedList) DeleteFrom(pos int) {
-	// Удалить элемент из позиции pos.
+func (l *LinkedList) At(pos int) Node {
+	// Getting an item from the pos position.
+	current := l.Head
+	for i := 0; i != pos && current.Next != nil; i++ {
+		current = current.Next
+	}
+	return *current
+}
+
+func (l *LinkedList) Add(val int) {
+	// Add an item to the end of the list.
+	current := l.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = &Node{Val: val, Next: nil}
+}
+
+func (l *LinkedList) Pop() {
+	// Remove an element from the end.
+	current := l.Head
+	if current.Next == nil {
+		return
+	}
+	for current.Next.Next != nil {
+		current = current.Next
+	}
+	current.Next = nil
 }
 
 func (l *LinkedList) UpdateAt(pos int, val int) {
-	// Значение на позиции pos сделать равным val.
+	// Make the value at the pos position equal to val.
 	current := l.Head
-	for i := 0; i < l.Size()-1; i++ {
-		if i == pos {
-			current.Val = val
-			break
-		}
+	for i := 0; i != pos && current.Next != nil; i++ {
 		current = current.Next
 	}
+	current.Val = val
 }
 
-// func NewFromSlise(s []int) *LinkedList {
-// 	// Создание связного списка из среза.
-// }
+func (l *LinkedList) DeleteAt(pos int) {
+	// Remove an item from the pos position.
+	current := l.Head
+	for i := 0; i != pos-1 && current.Next != nil; i++ {
+		current = current.Next
+	}
+	current.Next = current.Next.Next
+}
+
+func NewFromSlise(s []int) *LinkedList {
+	// Creating a linked list from a slice.
+	Head := Node{Val: s[0]}
+	current := &Head
+	for i := 0; i < len(s)-1; i++ {
+		current.Next = &Node{Val: s[i+1]}
+		current = current.Next
+	}
+	return &LinkedList{Head: &Head}
+}
 
 func main() {
-	list := New(5)
 
+	s := []int{-11, 22, 33, 44, -55, 66, -77}
+	list := NewFromSlise(s)
 	fmt.Println("size:", list.Size())
 	for i := 0; i < list.Size(); i++ {
 		fmt.Println("[", i, "]:", list.At(i))
 	}
 	fmt.Println("")
+	/*
+		list := New(5)
 
-	list.Add(100)
-	list.Add(200)
-	list.Add(300)
-	list.Add(400)
-	list.UpdateAt(0, -50)
-	list.UpdateAt(1, -5000)
+		fmt.Println("size:", list.Size())
+		for i := 0; i < list.Size(); i++ {
+			fmt.Println("[", i, "]:", list.At(i))
+		}
+		fmt.Println("")
 
-	fmt.Println("size:", list.Size())
-	for i := 0; i < list.Size(); i++ {
-		fmt.Println("[", i, "]:", list.At(i))
-	}
-	fmt.Println("")
+		list.Add(100)
+		list.Add(200)
+		list.Add(300)
+		list.Add(400)
+		list.UpdateAt(0, -50)
+		list.UpdateAt(1, -5000)
 
-	list.Pop()
-	list.Pop()
+		fmt.Println("size:", list.Size())
+		for i := 0; i < list.Size(); i++ {
+			fmt.Println("[", i, "]:", list.At(i))
+		}
+		fmt.Println("")
 
-	fmt.Println("size:", list.Size())
-	for i := 0; i < list.Size(); i++ {
-		fmt.Println("[", i, "]:", list.At(i))
-	}
-	fmt.Println("")
+		list.Pop()
+		list.Pop()
+
+		fmt.Println("size:", list.Size())
+		for i := 0; i < list.Size(); i++ {
+			fmt.Println("[", i, "]:", list.At(i))
+		}
+		fmt.Println("")
+
+		list.DeleteAt(5)
+		fmt.Println("size:", list.Size())
+		for i := 0; i < list.Size(); i++ {
+			fmt.Println("[", i, "]:", list.At(i))
+		}
+		fmt.Println("")
+	*/
 }
