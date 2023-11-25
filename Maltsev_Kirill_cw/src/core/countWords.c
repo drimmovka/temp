cfb074777fa279498c1dd97fdb3576b3fb214391
KirillMaltsev3341 <144445070+KirillMaltsev3341@users.noreply.github.com>
2023-11-25 21:57:46 +0300

    Add files via upload

---
+++ Maltsev_Kirill_cw/src/core/countWords.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "structures.h"
+#include "bst.h"
+
+void insertWords(Bst* bst, char* line) {
+    char* sep = "., \f\n\r\t\v";
+    char* substr;
+
+    substr = strtok(line, sep);
+    while (substr != NULL) {
+        while (isspace(*substr)) {
+    		substr++;
+		}
+        insert(bst, substr);
+        substr = strtok(NULL, sep);        
+    }
+}
+
+void fillBstByWords(Bst* bst, Text* t) {
+    for (int i=0; i<t->sentencesNumber; i++) {
+        char* cpy = (char*) calloc(strlen(t->sentences[i].line)+1, sizeof(char));
+        strcpy(cpy, t->sentences[i].line);
+        insertWords(bst, cpy);
+    }
+}
+
+void countWords(Text* t) {
+    Bst* bst = createBst();
+    fillBstByWords(bst, t);
+    InorderTraversal(bst);
+}
---
+++ Maltsev_Kirill_cw/src/header/replaceNonAlphaWithCode.h
@@ -0,0 +1,6 @@
+#ifndef REPLACE_NON_ALPHA_WITH_CODE_H
+#define REPLACE_NON_ALPHA_WITH_CODE_H
+
+void replaceNonAlphaWithCode(Text*);
+
+#endif
