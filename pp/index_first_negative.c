fbb2a2f1ca9b3a62729d9064fe75763a90a9936b
KirillMaltsev3341 <144445070+KirillMaltsev3341@users.noreply.github.com>
2023-11-30 14:24:16 +0300

    Delete pp directory

--- pp/index_first_negative.c
+++
@@ -1,9 +0,0 @@
-#include "index_first_negative.h"
-
-int index_first_negative(int arr[]) {
-  int i = 0;
-  while (arr[i] >= 0) {
-    i++;
-  }
-  return i;
-}
--- pp/sum_between_negative.h
+++
@@ -1,7 +0,0 @@
-#ifndef SUM_BETWEEN_NEGATIVE_H
-#define SUM_BETWEEN_NEGATIVE_H
-
-unsigned int sum_between_negative(int arr[], int size);
-
-
-#endif
